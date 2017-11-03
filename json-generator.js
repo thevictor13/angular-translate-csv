9f41279e3981e45364b68c2c80745368ae40df5b
Jérémy LANDON <jeremy.landon@outlook.com>
2017-11-03 19:23:10 +0100

    indent json result

--- json-generator.js
+++ json-generator.js
@@ -4,6 +4,7 @@ var Config = require(path.resolve(process.cwd(), process.argv[2]));
 var CsvParser = require('./csv-parser');
 var DirManager = require('./dir-manager');
 var StringCheck = require('./string-check');
+var JsonFormat = require('./json-format');
 
 module.exports = {
     generateJson: generateJson
@@ -26,8 +27,9 @@ function generateJson() {
                 
                 (function (translations, filePath) {
                     DirManager.ensureDirectoryExistence(filePath);
-                    
-                    fs.writeFile(filePath, JSON.stringify(translations), function (error) {
+                    var jsonContent = JsonFormat(translations);
+
+                    fs.writeFile(filePath, jsonContent, function (error) {
                         if (error) {
                             console.error('write error:  ' + error.message);
                         }
